28248776c5ee119663d88e66648da8e5d3ce84cf
Muneerabbas <muneer.abbas9595@gmail.com>
2026-04-03 15:10:13 +0530

    db-removal-completed

--- backend/db/migrate_004_role_hierarchy.sql
+++
@@ -1,4 +0,0 @@
--- Adds a hierarchy tier column to the company_roles table
--- which will synchronize across all users with that role
-
-ALTER TABLE company_roles ADD COLUMN IF NOT EXISTS hierarchy_tier INTEGER NOT NULL DEFAULT 0;
